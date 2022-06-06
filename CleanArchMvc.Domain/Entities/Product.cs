75d51290dc1f6a4e623f5e4016fcbf211e141ca3
bsfbruno <brunosuportefs@gmail.com>
2022-06-06 08:50:13 -0300

    Criando as entidades do modelo de dominio

---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public string image { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
