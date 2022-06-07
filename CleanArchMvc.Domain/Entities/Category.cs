8c3f41ed04d1389776955eeb79c40074d15ce27f
bsfbruno <brunosuportefs@gmail.com>
2022-06-07 08:36:04 -0300

    Enriquecendo modelo de dominio Category e criando Exception customizada

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,41 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    internal class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
 
-        public string Name { get; set; }
+        public Category(string name)
+        {
+            ValidateDomainName(name);
+        }
+        public Category(int id, string name)
+        {
+            ValidateDomainId(id);
+            ValidateDomainName(name);
+        }
+
+        public void update(string name)
+        {
+            ValidateDomainName(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomainName(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name. Name is too short");
+
+            Name = name;
+        }
+
+        private void ValidateDomainId(int id)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+
+            Id = Id;
+        }
     }
 }
