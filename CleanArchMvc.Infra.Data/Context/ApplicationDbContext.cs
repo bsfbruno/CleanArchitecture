afb8e9be8b259723cb1d754ba40961a5b3eb2a86
bsfbruno <brunosuportefs@gmail.com>
2022-06-27 13:50:06 -0300

    Implementando o Repository de Category

--- CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace CleanArchMvc.Infra.Data.Context
         }
 
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> products { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         //Configuração para utilizar FluentAPI
         protected override void OnModelCreating(ModelBuilder builder)
