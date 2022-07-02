6f94c4b7481b75bc56e7f3563323044383906ab2
bsfbruno <brunosuportefs@gmail.com>
2022-07-02 10:11:35 -0300

    Implementando os Handlers para Commands e Queries

--- CleanArchMvc.Application/Products/Commands/ProductUpdateCommand.cs
+++ CleanArchMvc.Application/Products/Commands/ProductUpdateCommand.cs
@@ -1,6 +1,6 @@
 ﻿namespace CleanArchMvc.Application.Products.Commands
 {
-    internal class ProductUpdateCommand : ProductCommand
+    public class ProductUpdateCommand : ProductCommand
     {
         public int Id { get; set; }
     }
